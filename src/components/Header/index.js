7f81aa92d933653a7d372e4e17377f8f1673b9c3
lucaspires-source <60105171+lucaspires-source@users.noreply.github.com>
2021-02-26 15:28:56 -0300

    Finished header component

---
+++ src/components/Header/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { AppBar, Toolbar, Typography } from '@material-ui/core'
+const Header = () => {
+    return (
+        <section>
+            <AppBar position="static">
+                <Toolbar >
+                    <Typography variant="h6" color="inherit">
+                        Pythagoras Calculator
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+        </section>
+    )
+}
+
+export default Header
